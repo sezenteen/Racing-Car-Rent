191327fcd4b6439010ffe0d63e1751efb88c9005
sezenteen <sezenteen@gmail.com>
2024-10-15 19:45:42 +0800

    first commit

---
+++ src/main/java/com/example/racingcarrent/RacingCarRentApplication.java
@@ -0,0 +1,13 @@
+package com.example.racingcarrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RacingCarRentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RacingCarRentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/racingcarrent/RacingCarRentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.racingcarrent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RacingCarRentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
